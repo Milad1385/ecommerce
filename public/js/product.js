36a68fd75adae7a0884687ca278992e42edab339
Milad salamian <miladsalami1385@gmail.com>
2024-06-12 04:46:54 -0700

    add product slider

--- public/js/product.js
+++ public/js/product.js
@@ -33,4 +33,31 @@ window.addEventListener("load", () => {
       swiper: secondSlider,
     },
   });
+
+  const categorySlider = new Swiper(".product-slider", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+  });
 });
